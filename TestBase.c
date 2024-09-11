3603bc31215f99c2840d403d6063aada286b5980
pbrazzle <prb50@pitt.edu>
2024-09-11 12:58:03 -0400

    500

--- TestBase.c
+++ TestBase.c
@@ -4,7 +4,7 @@
 Graph g;
 
 void init() {
-    int vertices = 200;  // Number of vertices in the graph
+    int vertices = 500;  // Number of vertices in the graph
     initializeGraph(&g, vertices);
 
     // Adding edges to the graph
